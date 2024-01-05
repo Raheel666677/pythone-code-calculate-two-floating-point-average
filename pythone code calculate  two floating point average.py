540ab38496b0408927b84a64371830cf6f691e4a
Raheel666677 <153253861+Raheel666677@users.noreply.github.com>
2024-01-05 09:25:43 +0500

    Add files via upload

---
+++ pythone code calculate  two floating point average.py
@@ -0,0 +1,7 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+... number1 = float(input("Enter the first floating-point number: "))
+... number2 = float(input("Enter the second floating-point number: "))
+... average = (number1 + number2) / 2
+... print(f"The average of {number1} and {number2} is: {average:.2f}")
